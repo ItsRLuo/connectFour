ccb3f8a9e5f5ddf6e65b7d8ba544e06379c1c37e
rlplayer <richardchen922@gmail.com>
2013-11-23 16:35:25 -0500

    Game board; now start on implementing the game

--- application/models/match.php
+++ application/models/match.php
@@ -11,4 +11,28 @@ class Match  {
 	
 	public $match_status_id = self::ACTIVE;
 		
+	public $board_state;
+	
+	
+	public $match_arr = array();
+	  
+	function __construct() {
+		
+// 		for ($i = 0; $i < 7; $i++) {
+// 			array_push($match_arr, array());
+// 			for ($j = 0; $j < 6; $j++) {
+// 				array_push($match_arr[$i], "");
+// 			}
+// 		}
+// 		$board_state = serialize($match_arr);
+		
+	}
+	
+
+	
+	
+	
+
+
+	
 }
